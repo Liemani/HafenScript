97d17e8621e6a55ff8242b0eea90d898c3364a95
Liemani <49893103+Liemani@users.noreply.github.com>
2020-05-19 09:03:07 +0900

    Update Path.h
    minor fix with comment line

--- scripts/LMT/model/Path.h
+++ scripts/LMT/model/Path.h
@@ -2,9 +2,10 @@ class Path {
     constructor(string) {
         let _path = string
         
-        // public method
+        // public property
         this.depth = _path.split("/").length - 1
         
+        // public method
         this.pathForLoadByCurrentDepth = function(currentDepth) {
             let pathForLoadByCurrentDepth = _path
             for(let depth = 0; depth < currentDepth; depth++) {
