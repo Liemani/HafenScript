03df981cb43953530074ca7d15c12795cc4e6ba6
Liemani <parkjh2768@naver.com>
2020-09-05 11:34:41 +0900

    Update

--- Study/03_NumberConstructor.js
+++ Study/03_NumberConstructor.js
@@ -1,6 +1,6 @@
 class Size {
     constructor(width, height) {    // constructor(width = 0, height = 0) {
-        this.width = Number(width) ? Number(width) : 0    //  if height is undefined then Number(undefined) return 0
+        this.width = Number(width) ? Number(width) : 0    // Number(undefined) { width === undefined ? return 0 }
         this.height = Number(height) ? Number(height) : 0    // if the argument of Number function can't to be converted to Number type then Number function return NaN
     }
 }
